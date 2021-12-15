769ff789a1def9e85d39694d397754bcd1d8af37
Jonathan Alvarado-Gomez <jonathan.alvaradogomez@revature.netgit config --global user.email jonathan.alvaradogomez@revature.netgit config --global user.email alvarado.jntn@gmail.comgit config --global user.name Jonathan>
2021-12-14 21:18:42 -0800

    added a comment

--- src/main/java/com/alvarado/Challenge2/RecursionStaircase.java
+++ src/main/java/com/alvarado/Challenge2/RecursionStaircase.java
@@ -4,6 +4,9 @@ public class RecursionStaircase {
 
     public static Integer waysToClimb (int n) {
 
+        // after writing out by hand the first 6 staircases, it appears the pattern follows the
+        // Fibonacci sequence, which is a recursive function.
+
         if (n == 0 || n == 1) {
             return 1;
         } else {
